793544facf7067f290bc46d50e307f59c37bbe6b
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-04-29 13:49:00 -0700

    Update main file.py

--- main file.py
+++ main file.py
@@ -5,44 +5,41 @@ def key_main() :
         def key_compiler(command_line):
             def function_reader(a):
                 x = 0
-                    for b in a:
-                        if b == "(":
-                            parameter_begin = x
-                        if b == ")":
-                            try:
-                                parameter_begin
-                            except NameError:
-                                print("""SyntaxError:  parameter beginning not found:""", a)
-                                print("""                                        """
-                                   
-                            parameter_end = x  
-                        x += 1
-            x = 0
-            for a in command_line:
-                length = 0
                 for b in a:
-                    length += 1
+                    if b == "(":
+                        parameter_begin = x
+                    if b == ")":
+                        try:
+                            if parameter_begin > 10:
+                                pass
+                        except NameError:
+                            print("""SyntaxError:  parameter beginning not found:""", a)
+                        else:     
+                            parameter_end = x  
+                return a[parameter_begin + 1, parameter_end - 1]
+            #begin compiling
+            for a in command_line:
+                values = {}
+                length = len(a)
                 if "print" in a:
-                    x = 0
-                    for b in a:
-                        if b == "(":
-                            parameter_begin = x
-                        if b == ")":
-                            try:
-                                if parameter_begin > 10:
-                                    pass
-                            except NameError:
-                                print("""SyntaxError:  parameter beginning not found:""", a)
-                            else:
-                                parameter_end = x
+                    print(function_reader(a))
+                if "make_int" in a:
+                    try:
+                        int(function_reader(a))
+                    except NameError:
+                        print("Value isn't int!", a)
+                if "=" in a:
+                    
                         
-                        x += 1
-                x += 1
+        #basic terminal
         in = input()
         if in == "code":
             while in != "close":
                 in = input()
                 command_line.append(in)
+                newfile = open(input("File Name:  "), "w+")
+                for a in command_line:
+                    
         if in == "compile":
             if command_line != []:
                 print("Compiling...")
