68e780dc2ecf08f6aac695b1fcdf8f189eee3183
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-04-30 08:36:25 -0700

    Update main file.py

--- main file.py
+++ main file.py
@@ -16,31 +16,34 @@ def key_main() :
                             print("""SyntaxError:  parameter beginning not found:""", a)
                         else:     
                             parameter_end = x  
-                return a[parameter_begin + 1, parameter_end - 1]
+                return a[parameter_begin : parameter_end]
             #begin compiling
+            value_size = 0
             for a in command_line:
                 values = {}
                 length = len(a)
                 if "print" in a:
                     print(function_reader(a))
-                if "make_int" in a:
+                if "=" in a:
+                    x = 0
+                    for b in a:
+                        if b == "=":
+                            values[a[-1:x]] = a[x:(len(a)+1)]
+                            break
+                        x += 1
+                if "make_int" in a:                
                     try:
                         int(function_reader(a))
                     except NameError:
-                        print("Value isn't int!", a)
-                if "=" in a:
-                    
-                        
+                        print("Value isn't int!")
         #basic terminal
-        in = input()
-        if in == "code":
-            while in != "close":
+        whattodo = input()
+        if whattodo == "code":
+            while whattodo != "close":
                 in = input()
                 command_line.append(in)
                 newfile = open(input("File Name:  "), "w+")
-                for a in command_line:
-                    
-        if in == "compile":
+        if whattodo == "compile":
             if command_line != []:
                 print("Compiling...")
                 key_compiler(command_line)
