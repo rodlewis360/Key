71ac1ac048ececc132208302f98df418aee7ce18
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-05-01 08:55:28 -0700

    Added save path

--- main file.py
+++ main file.py
@@ -1,5 +1,6 @@
 #!usr\bin\python
-def key_main() :
+dir = "C:/%USERNAME%/Downloads/"
+def key_main(dir) :
     while True:
         command_line = []
         def key_compiler(command_line):
@@ -20,18 +21,19 @@ def key_main() :
             #begin compiling
             value_size = 0
             for a in command_line:
+                #setting up vital variables
                 values = {}
                 length = len(a)
                 if "print" in a:
                     print(function_reader(a))
-                if "=" in a:
+                elif "=" in a:
                     x = 0
                     for b in a:
                         if b == "=":
                             values[a[-1:x]] = a[x:(len(a)+1)]
                             break
                         x += 1
-                if "make_int" in a:                
+                elif "make_int" in a:                
                     try:
                         int(function_reader(a))
                     except NameError:
@@ -40,13 +42,14 @@ def key_main() :
         whattodo = input()
         if whattodo == "code":
             while whattodo != "close":
+                import os.path
                 in = input()
                 command_line.append(in)
-                newfile = open(input("File Name:  "), "w+")
+                newfile = open(os.path.join(dir + input("File Name:  ") + ".txt"), "w+")
         if whattodo == "compile":
             if command_line != []:
                 print("Compiling...")
                 key_compiler(command_line)
-            if command_line == []:
+            else:
                 print("No command line detected.")
             
